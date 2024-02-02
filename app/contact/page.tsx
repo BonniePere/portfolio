9f49fcfec86567759a7c2e395c154b28d511c618
Bonnie Perez <bonnieperez472@proton.me>
2024-02-02 16:01:53 +0100

    Update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,10 +5,13 @@ import Container from '../components/Container';
 import Button from '../components/Button';
 
 const page = () => {
+  let inputClasses =
+    'border py-3 px-3 bg-slate-100 outline-none focus:border-primary-100';
+
   return (
     <section>
       <Container>
-        <section className="grid grid-cols-1 md:grid-cols-2">
+        <section className="grid grid-cols-1 md:grid-cols-[1fr_1.5fr] py-10 gap-10">
           <figure className="relative h-[25rem] md:h-[35rem]">
             <span className="absolute top-[80%] left-[50%] translate-x-[-50%] p-2 bg-black text-white text-5xl opacity-50">
               <span className="hidden md:block">
@@ -26,13 +29,43 @@ const page = () => {
           </figure>
 
           <form
-            action=""
-            className="text-center flex flex-col justify-center py-10 px-10"
+            action="https://formsubmit.co/your@email.com"
+            method="POST"
+            className="text-center flex flex-col justify-center py-10 gap-10"
           >
-            <h2 className="text-2xl text-center ">
-              Contact Bonnie Perez to discuss your interior design need.
+            <h2 className="text-3xl text-center ">
+              <strong>Contact Bonnie Perez</strong> to <strong>discuss</strong>{' '}
+              your interior <strong>design need</strong>.
             </h2>
-
+            <div className="grid gap-4">
+              <input
+                className={inputClasses}
+                placeholder="Full name"
+                type="text"
+                name="name"
+                required
+              />
+              <input
+                className={inputClasses}
+                placeholder="Email address"
+                type="email"
+                name="email"
+                required
+              />
+              <input
+                className={inputClasses}
+                placeholder="Phone number"
+                type="text"
+                name="phone-number"
+                required
+              />
+              <textarea
+                className={inputClasses}
+                placeholder="What project do you need help with?"
+                name="what-project-do-you-need-help-with"
+                required
+              ></textarea>
+            </div>
             <div className="grid">
               <Button
                 type="submit"
